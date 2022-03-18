aa366ee86b0c75f17aa47ad070b4c90d3fe4d048
MichelleVandes <m.vandeschrick@codeur.online>
2022-03-18 09:19:50 +0000

    fonctionnel

--- src/components/App.js
+++ src/components/App.js
@@ -3,47 +3,43 @@ import { useState } from "react";
 import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 
-
-
 function App() {
-
   const [todo, setTodo] = useState([
     { id: 1, tache: "Bonjour monde" },
     { id: 2, tache: "Hello word" },
   ]);
-  
 
-  const handleDelete = id => {
+  const handleDelete = (id) => {
     const majTodo = [...todo];
-    const index = majTodo.findIndex(todo => todo.id === id);
+    const index = majTodo.findIndex((todo) => todo.id === id);
 
     majTodo.splice(index, 1);
 
     setTodo(majTodo);
   };
 
-  const handleAdd = todo => {
+  const handleAdd = (newTodo) => {
+    console.log("newTodo", newTodo);
     const majTodo = [...todo];
-    majTodo.push(todo);
+    majTodo.push(newTodo);
+    console.log("majTodo", majTodo);
 
-   setTodo(majTodo);
+    setTodo(majTodo);
   };
 
-  
-    const title = "Liste des tâches à faire";
+  const title = "Liste des tâches à faire";
 
-    return (
-      <div>
-        <h2>{title}</h2>
-        <TodoForm addNewTache={handleAdd} />
-        <ul>
-          {todo.map((todo) => (
-            <Todo key={todo.id} details={todo} onDelete={handleDelete} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-
+  return (
+    <div>
+      <h2>{title}</h2>
+      <TodoForm addNewTache={handleAdd} />
+      <ul>
+        {todo.map((todo) => (
+          <Todo key={todo.id} details={todo} onDelete={handleDelete} />
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -5,25 +5,27 @@ const TodoForm = (props) => {
 
   const handleChange = (event) => {
     console.log("target", event.currentTarget.value);
+    setnewTache(event.currentTarget.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("newTache", event.currentTarget.value);
+    console.log("newTache", newTache);
 
     const id = new Date().getTime();
-    const item = event.currentTarget.value;
+    const tache = newTache;
 
-    props.addNewTache({ id, item });
+    props.addNewTache({ id, tache });
 
     setnewTache("");
+    
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input
-          //
+          value={newTache}
           onChange={handleChange}
           type="text"
           placeholder="Ajouter une tâche"
